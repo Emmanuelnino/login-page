6d244bfd39917187686b8c66e46e46860a3c9493
emmanuelnino <emmanuel98tembo@gmail.com>
2023-08-09 05:22:54 -0700

    anime

--- src/App.jsx
+++ src/App.jsx
@@ -12,8 +12,9 @@ function App() {
         <Form />
       </div>
 
-      <div className='hidden lg:flex h-full items-center justify-center bg-gray-200'>
-    <div className="w-60 h-60 bg-gradient-to-tr from-violet-500 to-pink-500 rounded-full"/>
+      <div className='hidden relative lg:flex h-full items-center justify-center bg-gray-200'>
+    <div className="w-60 h-60 bg-gradient-to-tr from-violet-500 to-pink-500 rounded-full animate-spin"/>
+    <div className= "w-full h-1/2 absolute bottom-0 bg-white/10 backdrop-blur-lg"/>
       </div>
     </div>
   );
